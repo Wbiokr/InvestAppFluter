139e7e5db0f32f51b6a873fdcd0ae82e26f91aed
wbiokr <Wbiokr@hotmail.com>
2018-07-24 22:00:47 +0800

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair=new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
-      theme: new ThemeData(
-        primaryColor: Colors.orange,
-      ),
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Welcome to Flutter ccc'),
+          title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(
-            'adda, 90',
-            textDirection: TextDirection.rtl
-            ),
-          // child: new Text(
-          //   wordPair.asPascalCase,
-          //   textDirection: TextDirection.rtl,
-          // ),
-          
+          child: new Text('Hello是否是打发 World'),
         ),
       ),
     );
   }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState()=> new RandomWordsState();
 }
